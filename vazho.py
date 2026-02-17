8cb1eca386420ed1cd55f17f9d14caae95695f6c
v1ksssqqpon-oss <v1ksssqqpon@gmail.com>
2026-02-17 18:36:17 +0300

    Add files via upload

--- vazho.py
+++ vazho.py
@@ -11,7 +11,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 # --- НАСТРОЙКИ ---
 TOKEN = '8517678651:AAGWCBa2BsWTS7M9HzTo7JWet6encABiKWE'
 ADMIN_ID = 1655167987 
-URL = "https://mishaswaga.github.io/cvetibot/"
+URL = "https://v1ksssqqpon-oss.github.io/cveti/"
 REQUISITES = "💳 Карта Сбер: 0000 0000 0000 0000 (Михаил С.)"
 
 bot = Bot(token=TOKEN)
